13c603d64ace38bf1578f935284918ba48d36d59
Thomas Ribeiro <thomas10191@hotmail.com>
2021-06-16 14:18:45 -0300

    feat: add method post and find of user schema

---
+++ src/services/Users.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import User from '../models/Users';
+
+export default class Users {
+
+  public async add(req: Request, res: Response): Promise<Response> {
+    try {
+      console.log(await req.body)
+      const user = await User.create(req.body);
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json(error)     
+    }
+  }
+  public async getAll(req: Request, res: Response): Promise<Response> {
+    try {
+      console.log(await req.body)
+      const user = await User.find();
+      return res.status(200).send({user});
+    } catch (error) {
+      return res.status(400).send({error})       
+    }
+  }
+  public async getById(req: Request, res: Response): Promise<Response> {
+    return res.send("");
+  }
+  public async update(req: Request, res: Response): Promise<Response> {
+    return res.send("");
+  }
+  public async delete(req: Request, res: Response): Promise<Response> {
+    return res.send("");
+  }
+
+}
